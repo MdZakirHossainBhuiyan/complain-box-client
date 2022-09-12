85bdc02ba51ae15f2f184c8d8e7261fbfb02598d
MdZakirHossainBhuiyan <zakirhossaintuhin@gmail.com>
2022-09-12 11:50:23 +0600

    mail box updated

--- src/Components/UserQuery/UserQuery.js
+++ src/Components/UserQuery/UserQuery.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './UserQuery.css';
 import UserQueryCard from './UserQueryCard/UserQueryCard';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
 
 const UserQuery = () => {
     const [activeReadButton, setActiveReadButton] = useState(false);
@@ -8,9 +10,6 @@ const UserQuery = () => {
     const [readQuery, setReadQuery] = useState(null);
     const [unreadQuery, setUnreadQuery] = useState(null);
     const [loader, setLoader] = useState(false);
-
-    console.log('read', readQuery);
-    console.log('unread', unreadQuery);
 
     const handleRead = () => {
         (activeReadButton)?setActiveReadButton(false):setActiveReadButton(true);
@@ -36,9 +35,35 @@ const UserQuery = () => {
         fetchUserQuery();
     },[])
 
+    let totalMail = 0;
+    let readMail = 0;
+    let unreadMail = 0;
+
+    if(readQuery && unreadQuery){
+        readMail = readQuery?.length;
+        unreadMail = unreadQuery?.length;
+        totalMail = readMail + unreadMail;
+    }
+
     return (
         <section className='userQuery-section'>
             <div className='userQuery-leftDiv'>
+                <div className='countSection'>
+                    <div className='countSectionTitle'>
+                        <h4>Mails from our beloved citizens</h4>
+                    </div>
+                    <div className='countValueArea'>
+                        <div className='countValueStyle total'>
+                            <h5>Total: <span>{totalMail}</span></h5>
+                        </div>
+                        <div className='countValueStyle read'>
+                            <h5>Read: <span>{readMail}</span></h5>
+                        </div>
+                        <div className='countValueStyle unread'>
+                            <h5>Unread: <span>{unreadMail}</span></h5>
+                        </div>
+                    </div>
+                </div>
                 {
                     activeReadButton && readQuery?.map(query => <UserQueryCard query={query} />)
                 }
@@ -47,11 +72,23 @@ const UserQuery = () => {
                 }
             </div>
             <div className='userQuery-rightDiv'>
-                <h4>Query Type</h4>
-                <ul>
-                    <li><button onClick={handleUnread}>Unread</button></li>
-                    <li><button onClick={handleRead}>Read</button></li>
-                </ul>
+                <div className='queryTypeTitle'>
+                    <h4>Query Type</h4>
+                </div>
+                <div className='typeSelectBtn'>
+                    <div className='btnArea'>
+                        <div className='buttonIcon'>
+                            <MarkEmailUnreadIcon style={{"color": "blue", "fontSize": "20px"}} />
+                        </div>
+                        <button onClick={handleUnread}>Unread</button>
+                    </div>
+                    <div className='btnArea'>
+                        <div className='buttonIcon'>
+                            <MarkEmailReadIcon style={{"color": "green", "fontSize": "20px"}} />
+                        </div>
+                        <button onClick={handleRead}>Read</button>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- src/Components/UserQuery/UserQueryCard/UserQueryCard.js
+++ src/Components/UserQuery/UserQueryCard/UserQueryCard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../UserQuery.css';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 const UserQueryCard = ({query}) => {
 
@@ -28,12 +32,22 @@ const UserQueryCard = ({query}) => {
 
     return (
         <div className='userQueryCard-body'>
-            <div className='userQuery-subjectDiv'>
-                <p>{query.subject}</p>
-            </div>
             <div className='userQuery-contactDiv'>
-                <h4>Name: {query.userName}</h4>
-                <p>Email: {query.userEmail}</p>
+                <div className='userIcon'>
+                    <AccountCircleOutlinedIcon style={{"fontSize": "30px", "color": "orange"}}/>
+                </div>
+                <div className='userIcon'>
+                    <h4>{query.userName}</h4>
+                </div>
+                <div className='userIcon'>
+                    <CircleOutlinedIcon  style={{"fontSize": "10px", "color": "blue"}}/>
+                </div>
+                <div className='userIcon'>
+                    <p>{query.userEmail}</p>
+                </div>
+            </div>
+            <div className='userQuery-subjectDiv'>
+                <p>Subject: <span>{query.subject}</span></p>
             </div>
             <div className='userQuery-bodyDiv'>
                 <p>{query.mailBody}</p>
@@ -41,11 +55,11 @@ const UserQueryCard = ({query}) => {
             <div className='userQuery-ButtonDiv'>
                 {
                     (query.readingStatus==='Unread') &&
-                    <button onClick={() => handleReadingStatus(query._id)}>Mark As Read</button>
+                    <button onClick={() => handleReadingStatus(query._id)}>Mark As Read <MarkEmailReadIcon style={{"fontSize": "15px", "marginLeft": "10px"}} /></button>
                 }
                 {
                     (query.readingStatus==='Read') &&
-                    <button onClick={() => handleDeleteQuery(query._id)}>Delete Query</button>
+                    <button onClick={() => handleDeleteQuery(query._id)}>Delete Query <DeleteOutlinedIcon style={{"fontSize": "15px", "marginLeft": "10px"}} /></button>
                 }
             </div>
         </div>
